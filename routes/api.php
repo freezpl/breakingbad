eab5ace4bfbe0d8891971d29951b878244057392
freezpl <freezpl@gmail.com>
2020-11-16 17:17:08 +0200

    setup laravel passport and users authentication pages

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,17 @@ use Illuminate\Support\Facades\Route;
 
 
 //api
-Route::get('episodes', 'Api\EpisodesController@list');
-Route::get('episodes/{id}', 'Api\EpisodesController@getById')->where('id', '[0-9]+');
+Route::group(['middleware' => ['auth:api']], function(){
+    Route::get('episodes', 'Api\EpisodesController@list');
+    Route::get('episodes/{id}', 'Api\EpisodesController@getById')->where('id', '[0-9]+');
+    
+    Route::get('characters', 'Api\CharactersController@list');
+    Route::get('characters/random', 'Api\CharactersController@random');
+    
+    Route::get('quotes', 'Api\QuotesController@list');
+    Route::get('quotes/random', 'Api\QuotesController@randomQuoteByAuthorName');
+});
 
-Route::get('characters', 'Api\CharactersController@list');
-Route::get('characters/random', 'Api\CharactersController@random');
-
-Route::get('quotes', 'Api\QuotesController@list');
-Route::get('quotes/random', 'Api\QuotesController@randomQuoteByAuthorName');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth:web'], 'prefix'=>'cabinet'], function () {
+    Route::get('/tokens', [App\Http\Controllers\HomeController::class, 'index']); 
+});
